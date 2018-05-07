5e645e53ac40fe7987f3179b8a5b49b4800b0772
Oli <Oli@Oli-VAIO>
2018-05-07 16:16:02 +0200

    jquery

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,15 +7,9 @@
         <title>Hotel-Spa Miramar</title>
 
         <!-- Bootstrap and my style-->
-        <link href="{{ url('/assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ url('/css/bootstrap.min.css') }}" rel="stylesheet">
         <link href="{{ url('/css/hotel_Spa.css') }}" rel="stylesheet">
 
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
     </head>
     <body>
         @include('navbar')
@@ -29,10 +23,9 @@
 
 
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="{{ url('/assets/bootstrap/js/bootstrap.min.js') }}"></script>
-        <script type="text/javascript" src="{{ url('/assets/js/videoclub.js') }}"></script>
+        <script src="{{ url('/js/jquery-3.3.1.min.js') }}"></script>
+        <script src="{{ url('/js/bootstrap.min.js') }}"></script>
+        <script type="text/javascript" src="{{ url('/js/hotelSpa.js') }}"></script>
     </body>
 </html>
 
