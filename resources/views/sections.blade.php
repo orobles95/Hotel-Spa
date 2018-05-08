7e582e3ac5fbb9a9e4d75140c9200c36060d05cf
Oscar Robles <oscarrobles27@gmail.com>
2018-05-08 17:59:25 +0200

    a

--- resources/views/sections.blade.php
+++ resources/views/sections.blade.php
@@ -1,44 +1,38 @@
-<div id="section_container">
-    <div id="section1">
-        <div id="instalaciones_container">
-            <div id="imgPortada_instalaciones_container">
-                <!--<img src="../images/instalaciones.jpg" class="img-rounded" alt="Instalaciones Spa">-->
-            </div>
-            <div id="text_instalaciones_container">
-                <h4 id="instalaciones_title">Instalaciones</h4>
-                <p id="instalaciones_description">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
-            </div>
-            <div id="imgCercle_instalaciones_container">
-                <!--<img src="../images/imagesTest/qh_grand_boras_spa1.jpg" class="img-circle" alt="Instalaciones Spa 2" width="304" height="236"> -->
-            </div>
+<div  class="container">
+    <div  class="row">
+        <div class="col-3 align-self-center">
+            <img src="images/instalaciones.jpg" class="img img-fluid rounded" alt="Instalaciones Spa">
+        </div>
+        <div  class="col-7">
+            <h4 id="instalaciones_title">Instalaciones</h4>
+            <p id="instalaciones_description">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+        </div>
+        <div  class="col-2 align-self-center">
+            <img src="images/instalaciones_circle.jpg" class="img img-fluid rounded-circle" alt="Instalaciones Spa 2">
         </div>
     </div>
-    <div id="section2">
-        <div id="tratamientos_container">
-            <div id="imgPortada_tratamientos_container">
-                <!--<img src="../images/instalaciones.jpg" class="img-rounded" alt="Instalaciones Spa">-->
-            </div>
-            <div id="text_tratamientos_container">
-                <h4 id="tratamientos_title">Instalaciones</h4>
-                <p id="tratamientos_description">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
-            </div>
-            <div id="imgCercle_tratamientos_container">
-                <!--<img src="../images/imagesTest/qh_grand_boras_spa1.jpg" class="img-circle" alt="Instalaciones Spa 2" width="304" height="236"> -->
-            </div>
+    <div id="tratamientos_container">
+        <div id="imgPortada_tratamientos_container">
+            <img src="images/tratamientos.jpg" class="img img-fluid rounded" alt="Tratamientos Spa">
+        </div>
+        <div id="text_tratamientos_container">
+            <h4 id="tratamientos_title">Trataminetos</h4>
+            <p id="tratamientos_description">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+        </div>
+        <div id="imgCercle_tratamientos_container">
+            <img src="images/tratamientos_circle.jpg" class="img img-fluid rounded-circle" alt="Tratamientos Spa 2">
         </div>
     </div>
-    <div id="section3">
-        <div id="socios_container">
-            <div id="imgPortada_socios_container">
-                <!--<img src="../images/instalaciones.jpg" class="img-rounded" alt="Instalaciones Spa">-->
-            </div>
-            <div id="text_socios_container">
-                <h4 id="socios_title">Instalaciones</h4>
-                <p id="socios_description">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
-            </div>
-            <div id="imgCercle_socios_container">
-                <!--<img src="../images/imagesTest/qh_grand_boras_spa1.jpg" class="img-circle" alt="Instalaciones Spa 2" width="304" height="236"> -->
-            </div>
+    <div id="socios_container">
+        <div id="imgPortada_socios_container">
+            <img src="images/socios.jpg" class="img img-fluid rounded" alt="Socios Spa">
+        </div>
+        <div id="text_socios_container">
+            <h4 id="socios_title">Socios</h4>
+            <p id="socios_description">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+        </div>
+        <div id="imgCercle_socios_container">
+            <img src="images/socios_circle.jpg" class="img img-fluid rounded-circle" alt="Socios Spa 2">
         </div>
     </div>
 </div>
