659f0617f298a2adabe39aa7bab9c0c90bdd4085
Carlos <Carlos@LAPTOP-9J2JFI70>
2018-05-22 19:43:44 +0200

    Prueba de Modal

---
+++ resources/views/calendario.blade.php.php
@@ -0,0 +1,37 @@
+
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+        
+        
+        <div class="modal fade" id="miModalModPerGrups" role="dialog">
+             
+        <div class="modal-dialog">
+        <div class="modal-content">   
+        <form>
+            
+        <label>Selecciona una fecha: <label>
+     
+        <input type="text" name="datetimes" />
+        
+        <select>
+                    <option value="habiacion" >Habitacion</option>
+        </select>
+        
+            
+            
+         <div class="text-right darkColor">
+            <button id="reservar" class="btn btn-success" type="submit">Reservar</button>
+        </div>   
+        </form>
+        </div> 
+        </div>
+             </div>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+        <script type="text/javascript" src="{{ url('/js/hotelSpa.js') }}"></script>
+        
+        
+ 
+
+
+
