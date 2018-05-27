12067b0df59aa79b6a11236210ded04b14af48c3
Oli <Oli@Oli-VAIO>
2018-05-27 13:42:19 +0200

    Merge origin/master
    Conflicts:
    	resources/lang/en/habitaciones.php

--- resources/lang/en/habitaciones.php
+++ resources/lang/en/habitaciones.php
@@ -22,18 +22,4 @@ return [
     'nombre_5' => 'BLUE ROYAL',
     'titulo_5' => 'Standar Room',
     'descripcion_5' => 'The comfort available to everyone with the best hotel tradition.'
-    'titulo' => 'Rooms',
-    'bienvenida' => 'With an elegant and fresh design, based on the use of noble materials, our rooms will transport you
-                 to the origins of rest. A unique essence that guarantees a vital stay full of peace and comfort.',
-    'titulo_1' => 'MADAME BUTTERFLY',
-    'descripcion_1' => 'Presidential Suite',
-    'descripcion_1_2' => 'An oasis of peace and wellbeing inspired by the famous opera by Giacomo Puccini.',
-    
-    
-    'titulo_2' => 'MADAME BUTTERFLY',
-    'detalles_2' => '92 m²',
-    'descripcion_2' => 'Suite Presidencial',
-    'descripcion_2_2' => 'Un oasis de paz y bienestar inspirado en la famosa ópera de Giacomo Puccini.',
-    'reservarBtn' => 'Book Now',
-    'descubrelaBtn' => 'Discover it →'
 ];
