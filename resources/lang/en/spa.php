884fbd00341780c5934e6ad0144be65ad4058d06
Oli <Oli@Oli-VAIO>
2018-05-28 18:04:11 +0200

    balneario traducido

---
+++ resources/lang/en/spa.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'tituloPestanya' => 'Spa',
+    'tituloPagina' => 'SPA',
+    'bienvenida' => 'Enter our Spa and immerse yourself in an oasis full of sensations and indescribable pleasures. An authentic
+                    temple of well-being that recovers the ancestral legacy of wine therapy so that you can enjoy a vital experience.',
+    'reservarBtn' => 'Reserve hour',
+    'descubrelaBtn' => 'Discover it →',
+    'capacidad_1' => 'Shared group',
+    'nombre_1' => 'HOT SPRINGS',
+    'titulo_1' => 'Thermal Circuit',
+    'descripcion_1' => 'Close your eyes and embark on a vital journey to the roots of pleasure through a unique and intimate circuit
+                        thermal that will become a gift for your senses.',
+    'capacidad_2' => 'Shared group',
+    'nombre_2' => 'NATURAL SPA',
+    'titulo_2' => 'Termal Aqua',
+    'descripcion_2' => 'Enter a silent nature that collects pleasures for your senses. Body, mind and soul meet again
+                        in total harmony in a space where time stands still.',
+    'capacidad_3' => 'Individual / Couples',
+    'nombre_3' => 'PURIFYING SAUNA',
+    'titulo_3' => 'Divinium',
+    'descripcion_3' => 'Disconnect in the sauna, with a delicate bath to the wine, in the relaxation area or with the exclusive treatments
+                        individual or in pairs.'
+];
+
+
+
