bdfdf885601c2eb8f907a25e2291fd09cc5aee26
Oli <Oli@192.168.1.37>
2018-06-01 21:29:50 +0200

    susscribe

--- app/Http/Controllers/suscribeController.php
+++ app/Http/Controllers/suscribeController.php
@@ -15,24 +15,23 @@ class suscribeController extends Controller {
 
     public function afegirSuscriptor(Request $request) {
 
-        //$usuari = Suscrito::where('email', $request->suscribeEmail)->first();
+        $usuari = Suscrito::where('email', $request->suscribeEmail)->first();
         //$usuari = DB::select("SELECT * FROM suscritos WHERE email = '" . $request->suscribeEmail."'");
 
 
-        /* if ($usuari == null) {
-          $usuari = new Suscrito;
-          $usuari->nombre = $request->suscribeNombre;
-          $usuari->apellidos = $request->suscribeApellidos;
-          $usuari->email = $request->suscribeEmail;
-          $usuari->save();
+        if ($usuari == null) {
+            $usuari = new Suscrito;
+            $usuari->nombre = $request->suscribeNombre;
+            $usuari->apellidos = $request->suscribeApellidos;
+            $usuari->email = $request->suscribeEmail;
+            $usuari->save();
 
-          Notification::success("Se ha suscrito correctamente.");
-          return back();
-          } else {
-          Notification::error("Error!!! Este usuario ya esta suscrito.");
-          return back();
-          } */
-        echo "bla";
+            Notification::success("Se ha suscrito correctamente.");
+            return back();
+        } else {
+            Notification::error("Error!!! Este usuario ya esta suscrito.");
+            return back();
+        }
     }
 
 }
